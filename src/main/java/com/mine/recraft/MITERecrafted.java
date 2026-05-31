8d4c247e65bf9bb680953b0812831804c975760e
FJKiXfaR <073224131@suzt.edu.cn>
2026-06-01 01:16:30 +0800

    完善血量逻辑，禁用生命自然恢复

--- src/main/java/com/mine/recraft/MITERecrafted.java
+++ src/main/java/com/mine/recraft/MITERecrafted.java
@@ -18,7 +18,7 @@ public class MITERecrafted implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
 		LOGGER.info("Hello Fabric world!");
+		PlayerManager.register();
 	}
 }
